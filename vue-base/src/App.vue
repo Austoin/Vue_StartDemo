88ae158e408f8ff9f16159d48f9a99ec37dcc510
Austoin <liuzhigui_2023@qq.com>
2025-10-20 11:52:47 +0800

    10-20

--- vue-base/src/App.vue
+++ vue-base/src/App.vue
@@ -2,6 +2,7 @@
   import index_1 from './compoennts/index_1.vue'
   import index_2_ifshow from './compoennts/index_2_ifshow.vue'
   import index_3_list from './compoennts/index_3_list.vue'
+  import index_4_keyDemo from './compoennts/index_4_keyDemo.vue'
 </script>
 
 <template>
@@ -11,5 +12,7 @@
   <index_2_ifshow />
 
   <index_3_list />
+
+  <index_4_keyDemo />
 
 </template>
---
+++ vue-base/src/compoennts/index_4_keyDemo.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    data() {
+        return {
+            name: ["Austoin", "Tom", "Kiti"], 
+
+            cnt: "content", 
+
+            color: "mr"
+        }
+    },
+}
+</script>
+
+<template>
+    <div :class="cnt">
+
+        <!-- 主要是在vue中调换列表元素的信息会重新渲染，使用key就只会调用顺序达到目的了 -->
+
+        <h2>key在v-for的应用</h2>  
+
+        <p v-for="(value, index) in name" :key="index", :class="color">{{ value }}---{{ index }}</p>  <!-- key应用索引 -->
+
+        <!-- 这里看起来无变化，只是更新的时候不会就地更新重新渲染，而是通过key调换顺序减少内存消耗 -->
+
+    </div>
+</template>
+
+<style>
+    .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;   
+        justify-content: center;
+        
+        height: 50vh; 
+        border: 1px solid #000;
+    }
+
+    .mr {
+        color: blueviolet;
+        font-family: 'Times New Roman', Times, serif;
+        size: 50px;
+    }
+</style>
